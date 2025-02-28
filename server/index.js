d069d28347a5b707a7ce5d0b97a6c7d2469b8fd2
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2025-02-28 17:36:13 +0530

    env PORT

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 dotenv.config()
 const app=express()
-app.use(cors( { origin: ["http://localhost:5173"] , credentials: true, } ))
+app.use(cors( { origin: ["https://localhost:5173"] , credentials: true, } ))
 app.use(cookieParser())
 app.use(express.json())
 app.use("/auth", UserRouter)
