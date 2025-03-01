fd5367393ad623de0fade4706884e4bb6b5486f2
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2025-03-01 11:29:28 +0530

    main branch

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import { UserRouter } from "./routes/user.js"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 dotenv.config()
-const app=express()
-app.use(cors( { origin: ["https://localhost:5173"] , credentials: true, } ))
-app.use(cookieParser())
+import { UserRouter } from './routes/user.js'
+
+const app = express()
 app.use(express.json())
-app.use("/auth", UserRouter)
-mongoose.connect("mongodb://127.0.0.1:27017/authentication")
-app.listen(process.env.PORT, ()=>{
-    console.log(" Server is Running");
-    
+app.use(cors({
+    origin: ["http://master-auth-7hwv-ile00vbiz-rushikesh-avachats-projects-5cd454f2.vercel.app"],
+    credentials: true
+}))
+app.use(cookieParser())
+app.use('/auth', UserRouter)
+
+mongoose.connect('mongodb://127.0.0.1:27017/authentication')
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is Running")
 })
